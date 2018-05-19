8f101077e86f9f84c9fa508980b8ddee1b90d5a3
Tim Hall <Timmy@Tims-MacBook-Pro.local>
2018-05-19 15:37:46 +0100

    playing with gravity

--- main.js
+++ main.js
@@ -1,60 +1,80 @@
 const friction = 0.01
 const unit = 'px'
 const g = 9.81
+const timeInterval = 15
 
+var interval
 var mouseX, mouseY
 var boxHeight, boxWidth
 var container = document.getElementById('container')
 
 var pointer = newCircle(300, 100, 'pointer')
-
+var moving = newCircle(100, 100, 'moving')
+moving.yspeed = 0
 var fixies = []
 var movings = []
 
 
+document.addEventListener('mousemove', function (event) {
+    mouseX = event.clientX
+    mouseY = event.clientY
+})
 
 
+function start() { interval = setInterval(refresh, 15) }
 
-movings.push(newCircle(100, 100, 'moving'))
+// Refresh Screen function
+function refresh() {
+    pointer.moveAbsolute((mouseX - 10), (mouseY - 10), true)
+    moving.move()
+    mousespeed = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2)) / 4
+    if (mousespeed < 5) mousespeed = 5
+    x0 = x
+    y0 = y
+    movingBox.move()
+    //movingBox.element.style.left = 10 + 'px' 
+}
 
+//movings.push(newCircle(100, 100, 'moving'))
 
 // Create some fixed points
-fixies.push(newCircle(200, 200, 'fixedPoint'))
-fixies.push(newCircle(700, 400, 'fixedPoint'))
+fixies.push(newCircle(200, 500, 'fixedPoint', true))
+fixies.push(newCircle(700, 400, 'fixedPoint', true))
 
-function CheckCollision(moving, fixie) {
-    var r = (moving.offsetWidth / 2) + (fixie.offsetWidth / 2)
+function checkAllCollisions(obj){
+
+}
+
+function checkCollision(objA, objB) {
+    var r = (objA.offsetWidth / 2) + (objB.offsetWidth / 2)
     var d = getSpeed(fixie.x - (moving.x + moving.xspeed), fixie.y - (moving.y + moving.yspeed))
 
-    if (d < r) moving.collision(fixie)
+    if (d < r) {
+        
+    }
 }
 
-
-
-for (var f in fixies) {
-    console.log(fixies[f].x)
-}
-function CheckMovings(){
+function CheckMovings() {
     for (var m in movings) checkAMoving(m)
 }
 
-function checkAMoving(moving){
+function checkAMoving(moving) {
     for (var f in fixies) CheckCollision(moving, f)
-    
+
 }
 function getAngle(y, x) {
     return Math.atan2(y, x)
 }
 
-function getXspeed(speed, angle){
+function getXspeed(speed, angle) {
     return Math.cos(angle) * speed
 }
 
-function getYspeed(speed, angle){
+function getYspeed(speed, angle) {
     return Math.sin(angle) * speed
 }
 
-function getSpeed(xspeed, yspeed){
+function getSpeed(xspeed, yspeed) {
     return Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2))
 }
 
@@ -62,28 +82,54 @@ function newMoving(_xspeed = 0, _speed = 0) {
     var newC = newCircle
 }
 
-function newCircle(x, y, cls){
-    var cirle = document.createElement('div')
-    cirle.classList.add(cls)
-    container.appendChild(cirle)
+function newCircle(x, y, cls, fixed = false) {
+    var circle = document.createElement('div')
+    circle.classList.add(cls)
+    container.appendChild(circle)
 
-    cirle.move = function(_x, _y){
-        if (this.x != _x) {
-            this.style.left = _x + unit
-            this.x = _x
-        } 
+    circle.xspeed = 0
+    circle.yspeed = 0
+    circle.fixed = fixed
 
-        if (this.y != _y){
-            this.style.top = _y + unit
-            this.y = _y
+    circle.move = function() {
+      
+        if ((this.offsetTop + this.yspeed) > (container.offsetHeight - this.offsetHeight)) {
+                //this.style.top = (container.offsetHeight - this.element.offsetHeight) + unit
+                this.yspeed *= -0.8
         }
+        this.x += this.xspeed
+        this.y += this.yspeed
+        this.yspeed += 0.5
+        this.style.left = this.x + unit 
+        
+        this.style.top = this.y + unit
+        
     }
 
-    cirle.move(x, y)
-    cirle.xspeed = 0
-    cirle.yspeed = 0
+    circle.moveAbsolute = function (_x, _y, updateSpeed = false) {
+        if (this.x != _x) {
+            this.style.left = _x + unit
+            if (updateSpeed) this.xspeed = _x - this.x
+            this.x = _x
+        } else if (updateSpeed) this.xspeed = 0
+        
 
-    cirle.collision = function(obj){
+        if (this.y != _y) {
+            this.style.top = _y + unit
+            if (updateSpeed) this.yspeed = _y - this.y
+            this.y = _y
+        } else if (updateSpeed) this.yspeed = 0
+    }
+
+    circle.moveSpeed = function (_xspeed, _yspeed) {
+        this.xspeed = _xspeed
+        this.yspeed = _yspeed
+        this.move()
+    }
+
+    circle.moveAbsolute(x, y)
+
+    circle.collision = function (obj) {
         var angle = getAngle(this.xspeed, this.yspeed)
         var speed = getSpeed(this.xspeed, this.yspeed)
 
@@ -91,7 +137,7 @@ function newCircle(x, y, cls){
 
         var speedcol = getXspeed(speed, angle - anglecol)
     }
-    return cirle
+    return circle
 }
 
 function ReverseAngle(angle) {
@@ -100,7 +146,7 @@ function ReverseAngle(angle) {
     //if (modAngle < 0) 
 }
 
-function getBoxSize(){
+function getBoxSize() {
     boxWidth = container.offsetWidth
     boxHeight = container.offsetHeight
 }
@@ -108,7 +154,7 @@ function getBoxSize(){
 window.addEventListener('resize', getBoxSize)
 
 var movingBox = {
-    
+
     element: document.getElementsByClassName('moving')[0],
     speed: 0,
     angle: 0,
@@ -116,7 +162,7 @@ var movingBox = {
     yspeed: 0,
     x: 0,
     y: 0,
-    move: function() {
+    move: function () {
 
         var xdiff = getXspeed(this.speed, this.angle)
         var ydiff = getYspeed(this.speed, this.angle)
@@ -126,8 +172,8 @@ var movingBox = {
         if ((this.element.offsetLeft + xdiff) < 0) {
             this.element.style.left = '0'
             this.angle = this.angle < 0 ? -Math.PI - this.angle : Math.PI - this.angle
-            
-        } else if ((this.element.offsetLeft + xdiff) > (container.offsetWidth - this.element.offsetWidth)){
+
+        } else if ((this.element.offsetLeft + xdiff) > (container.offsetWidth - this.element.offsetWidth)) {
             this.element.style.left = (container.offsetWidth - this.element.offsetWidth) + unit
             this.angle = this.angle < 0 ? -Math.PI - this.angle : Math.PI - this.angle
         } else {
@@ -137,9 +183,9 @@ var movingBox = {
         if ((this.element.offsetTop + ydiff) < 0) {
             this.element.style.top = '0'
             this.angle *= -1
-        // } else if ((this.element.offsetTop + ydiff) > (container.offsetHeight - this.element.offsetHeight)) {
-        //     this.element.style.top = (container.offsetHeight - this.element.offsetHeight) + unit
-        //     this.angle *= -1
+            // } else if ((this.element.offsetTop + ydiff) > (container.offsetHeight - this.element.offsetHeight)) {
+            //     this.element.style.top = (container.offsetHeight - this.element.offsetHeight) + unit
+            //     this.angle *= -1
         } else {
             this.element.style.top = (this.element.offsetTop + ydiff) + unit
         }
@@ -154,48 +200,30 @@ var x, y, x0, y0
 var mousespeed
 var listen = true
 
-document.addEventListener('mousemove', function(event){
-    mouseX = event.clientX
-    mouseY = event.clientY
-    
-})
-movingBox.element.addEventListener('mousemove', function(event){
+
+movingBox.element.addEventListener('mousemove', function (event) {
     if (listen) {
         //window.alert(event.shiftKey)
         listen = false
     }
 })
 
-movingBox.element.addEventListener('mouseover', function(event){
- 
-        xbox = movingBox.element.offsetLeft + movingBox.element.offsetWidth / 2
-        ybox = movingBox.element.offsetTop + movingBox.element.offsetHeight / 2
-        movingBox.angle = getAngle(ybox - event.clientY, xbox - event.clientX)
-        //x0 = x
-        //y0 = y
-        movingBox.speed = mousespeed
-        // setTimeout(() => {
-        //    //document.getElementById('label').innerHTML = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2))
-        //    let s = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2)) / 4
-        //    movingBox.speed = s > 20 ? 20 : s
-        // }, 15);
+movingBox.element.addEventListener('mouseover', function (event) {
+
+    xbox = movingBox.element.offsetLeft + movingBox.element.offsetWidth / 2
+    ybox = movingBox.element.offsetTop + movingBox.element.offsetHeight / 2
+    movingBox.angle = getAngle(ybox - event.clientY, xbox - event.clientX)
+    //x0 = x
+    //y0 = y
+    movingBox.speed = mousespeed
+    // setTimeout(() => {
+    //    //document.getElementById('label').innerHTML = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2))
+    //    let s = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2)) / 4
+    //    movingBox.speed = s > 20 ? 20 : s
+    // }, 15);
 })
 
 
-function start(){
-    setInterval(refresh, 15)
-}
 
-function refresh() {
-    //window.alert('dddd')
-    pointer.move((mouseX - 10), (mouseY - 10))
-    
-    mousespeed = Math.sqrt(Math.pow(x0 - x, 2) + Math.pow(y0 - y, 2)) / 4
-    if (mousespeed < 5) mousespeed = 5
-    x0 = x
-    y0 = y
-    movingBox.move()
-    //movingBox.element.style.left = 10 + 'px'
-}
 
 
